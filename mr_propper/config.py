fc29a9a3dccb35fd0c22d16bd938ce71fc2e3cd3
Илья Лебедев <i.lebedev@bestdoctor.ru>
2019-10-27 16:32:24 +0300

    add all validators realization

--- mr_propper/config.py
+++ mr_propper/config.py
@@ -6,5 +6,19 @@ ATTRIBUTES_BLACKLIST = [
     'objects', 'post', 'count', 'all', 'exists',
     'filter', 'values_list', 'values', 'save',
 ]
+FORBIDDEN_ARGUMENT_TYPES = ['QuerySet', 'Model', 'Callable']
+FORBIDDEN_ATTR_CALLS_FOR_ARGS = {
+    # list mutating methods
+    'append',
+    'clear',
+    'extend',
+    'insert',
+    'pop',
+    'remove',
+    'reverse',
+    'sort',
+    # dict mutation_methods
+    'update',
+}
 
 TARGET_PYTHON_VERSION = os.environ.get('MR_PROPPER_TARGET_PYTHON_VERSION', '3.7')
--- mr_propper/pure_validators.py
+++ mr_propper/pure_validators.py
@@ -1,14 +1,17 @@
 import ast
 import builtins
-from typing import List, Optional
+from typing import List, Optional, Callable, cast
 
 from mr_propper.common_types import AnyFuncdef
-from mr_propper.config import CALLABLES_BLACKLIST, ATTRIBUTES_BLACKLIST
+from mr_propper.config import (
+    CALLABLES_BLACKLIST, ATTRIBUTES_BLACKLIST, FORBIDDEN_ARGUMENT_TYPES,
+    FORBIDDEN_ATTR_CALLS_FOR_ARGS)
 from mr_propper.utils.ast import (
     get_nodes_from_funcdef_body, is_imported_from_stdlib,
     get_local_var_names_from_funcdef,
 )
 from mr_propper.utils.python_naming import is_python_class_name
+
 
 BUILTINS_LIST = {b for b in dir(builtins) if not b.startswith('_')}
 
@@ -41,12 +44,18 @@ def uses_only_args_and_local_vars(
         if isinstance(c.func, ast.Name)
     ]
     local_vars_names = get_local_var_names_from_funcdef(funcdef_node)
+    subfunctions_arguments = {
+        a.arg
+        for n in get_nodes_from_funcdef_body(funcdef_node, [ast.FunctionDef, ast.AsyncFunctionDef])
+        for a in n.args.args  # type: ignore
+    }
 
     nonlocal_names = list(
         set(all_names)
         - set(args_names)
         - set(local_vars_names)
         - set(called_names)
+        - subfunctions_arguments
         - BUILTINS_LIST,
     )
     if allow_external_class_usage:
@@ -61,17 +70,64 @@ def uses_only_args_and_local_vars(
 
 
 def has_returns(funcdef_node: AnyFuncdef, file_ast_tree: Optional[ast.Module]) -> List[str]:
+    if not get_nodes_from_funcdef_body(funcdef_node, [ast.Return]):
+        return ['it has no returns']
     return []
 
 
 def not_mutates_args(funcdef_node: AnyFuncdef, file_ast_tree: Optional[ast.Module]) -> List[str]:
-    return []
+    args_names = {a.arg for a in funcdef_node.args.args}
+    mutating_actions = [  # noqa
+        (  # n +=
+            ast.AugAssign,
+            lambda n: True,
+            lambda n: n.target,
+        ),
+        (  # n[...] =
+            ast.Assign,
+            lambda n: (
+                n.targets
+                and isinstance(n.targets[0], ast.Subscript)
+                and isinstance(n.targets[0].value, ast.Name)
+                and n.targets[0].value.id in args_names
+            ),
+            lambda n: n.targets[0],
+        ),
+        (  # n.foo =
+            ast.Assign,
+            lambda n: n.targets and isinstance(n.targets[0], ast.Attribute),
+            lambda n: n.targets[0].value,
+        ),
+        (  # n.append/remove/...(...)
+            ast.Attribute,
+            lambda n: (
+                n.attr in FORBIDDEN_ATTR_CALLS_FOR_ARGS
+                and isinstance(n.value, ast.Name)
+                and n.value.id in args_names
+            ),
+            lambda n: n.value,
+        ),
+    ]
+
+    errors: List[str] = []
+    for node_type, node_validator, node_extractor in mutating_actions:
+        node_validator = cast(Callable[[ast.AST], bool], node_validator)
+        node_extractor = cast(Callable[[ast.AST], ast.AST], node_extractor)
+        for node in get_nodes_from_funcdef_body(funcdef_node, [node_type]):
+            if not node_validator(node):
+                continue
+            for name_node in [n for n in ast.walk(node_extractor(node)) if isinstance(n, ast.Name)]:
+                if name_node.id in args_names:
+                    errors.append(f'it mutates its own argument (on line {node.lineno})')
+    return errors
 
 
 def not_has_local_imports(
     funcdef_node: AnyFuncdef,
     file_ast_tree: Optional[ast.Module],
 ) -> List[str]:
+    if get_nodes_from_funcdef_body(funcdef_node, [ast.ImportFrom, ast.Import]):
+        return ['it has local imports']
     return []
 
 
@@ -79,11 +135,27 @@ def not_has_forbidden_arguments_types(
     funcdef_node: AnyFuncdef,
     file_ast_tree: Optional[ast.Module],
 ) -> List[str]:
-    return []
+    errors: List[str] = []
+    for argument in funcdef_node.args.args:
+        if not argument.annotation:
+            continue
+        for annotation_part_node in ast.walk(argument.annotation):
+            if not isinstance(annotation_part_node, ast.Name):
+                continue
+            type_without_prefix = annotation_part_node.id.split('.')[-1]
+            if type_without_prefix in FORBIDDEN_ARGUMENT_TYPES:
+                errors.append(f'it has type {type_without_prefix} in argument types')
+    return errors
 
 
 def not_uses_self_or_class_vars(
     funcdef_node: AnyFuncdef,
     file_ast_tree: Optional[ast.Module],
 ) -> List[str]:
-    return []
+    forbidden_names = {'self', 'cls', 'super'}
+
+    errors: List[str] = []
+    for name_node in get_nodes_from_funcdef_body(funcdef_node, [ast.Name]):
+        if name_node.id in forbidden_names:
+            errors.append(f'it uses {name_node.id} var')
+    return errors
