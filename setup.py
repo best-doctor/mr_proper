ece84a6b4278565965cc7d46ea7f9f58ff766b3c
melevir <melevir@gmail.com>
2020-05-06 09:05:16 +0300

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     keywords='typos',
-    version='0.0.4',
+    version='0.0.5',
     author='Ilya Lebedev',
     author_email='melevir@gmail.com',
     install_requires=[
